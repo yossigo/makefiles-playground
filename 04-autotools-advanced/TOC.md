7567d8f52eebc0e1ac644ffa4914195bed87d311
Yossi Gottlieb <yossigo@gmail.com>
2021-06-27 14:11:27 +0300

    Add configuration example.

--- 04-autotools-advanced/TOC.md
+++ 04-autotools-advanced/TOC.md
@@ -2,3 +2,5 @@
 * Properly use config.h
 * Use AC_CHECK_HEADERS
 * Use third party libs through pkg-config with config option
+
+* Build-time configuration vars
--- 04-autotools-advanced/myprog.c
+++ 04-autotools-advanced/myprog.c
@@ -2,10 +2,15 @@
 #include "config.h"
 #endif
 
+#ifdef HAVE_STDIO_H
+#include <stdio.h>
+#endif
+
 #include "mymodule.h"
 
 int main(int argc, char *argv[])
 {
+    printf("If I had a config file, it was %s/myprog.conf\n", CONFIG_DIR);
     module_func();
     return 0;
 }
